6999c2abd0171a27cbe7c7d5fab0e30f0a365621
axelrbe <ribeiro.axel.fabien@gmail.com>
2023-02-24 15:30:37 +0100

    creating repository

---
+++ app/src/main/java/com/example/lamzone/Repository/MeetingRepository.java
@@ -0,0 +1,25 @@
+package com.example.lamzone.Repository;
+
+import com.example.lamzone.Model.Meeting;
+import com.example.lamzone.Model.Room;
+import com.example.lamzone.Service.MeetingApiService;
+
+import java.util.List;
+
+public class MeetingRepository {
+
+    private final MeetingApiService apiService;
+
+    public MeetingRepository(MeetingApiService meetingApiService) {apiService = meetingApiService;}
+
+    public List<Meeting> getMeetingsList() {return apiService.getAllMeetings();}
+
+    public void addMeeting(Meeting meeting) {
+        apiService.createMeeting(meeting);
+    }
+
+    public void deleteMeeting(Meeting meeting) {
+        apiService.deleteMeeting(meeting);
+    }
+    public List<Room> getMeetingsRoomsList() {return apiService.getAllRooms();}
+}
